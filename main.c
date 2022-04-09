a4e7eb5f7535f0988e7a6da422fdf25e5a893f13
Accacio <raccacio@poli.ufrj.br>
2022-04-09 20:27:52 +0200

    train dataset and test

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 #include "neuron.h"
 #include "csv.h"
 
+#define Assert(value) if (!(value)){ int *p= 0; *p = 0;}
+
 
 int main(int argc, char *argv[]) {
     CSVData myCsvData;
     myCsvData.ncol = CSVgetColumns(argv[1]);
     myCsvData.nrow = 5;
-
     CSVallocateData(&myCsvData,128);
 
     int success = CSVread(argv[1],&myCsvData);
@@ -19,10 +20,7 @@ int main(int argc, char *argv[]) {
         printf("Exiting\n");
         exit(EXIT_FAILURE);
     }
-
     CSVprintData(myCsvData);
-
-    return 0;
 
     Neuron neuron;
 
@@ -30,17 +28,23 @@ int main(int argc, char *argv[]) {
     NeuronInitialize(&neuron, 2);
     neuron.lr=0.1;
     double  newWeights[]= {19,2};
-    double  dataInputs[]= {1, 2};
+    double  dataInputs[]= {4, 9};
     double  dataOutput = -1;
 
     NeuronUpdateWeightsFromArray(&neuron,newWeights);
 
-    for (int i = 0; i < 10; i++) {
+
+    for (int i = 0; i < myCsvData.nrow; i++) {
         printf("Train\n");
-        NeuronTrain(&neuron, dataInputs, dataOutput);
+        double  dataInputs[]= {atof(myCsvData.data[i][0]),atof(myCsvData.data[i][1])};
+        NeuronTrain(&neuron, dataInputs, atof(myCsvData.data[i][2]));
     }
+    /* inutil(); */
+    printf("\n");
+    printf("Input (%f,%f) Output %f",dataInputs[0],dataInputs[1],NeuronPredictFromArray(&neuron,dataInputs));
 
     /* TODO(accacio) free csvData */
     NeuronDelete(&neuron);
     exit(EXIT_SUCCESS);
+    return 0;
 }
